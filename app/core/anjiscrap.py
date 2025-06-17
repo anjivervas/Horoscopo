3fe29122f92b501c9a84feca7c41cb99c45d5711
anjivervas <anjivervas03@gmail.com>
2025-06-17 15:53:31 -0400

    Actualizacion

--- app/core/anjiscrap.py
+++ app/core/anjiscrap.py
@@ -1,27 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 
-url='https://www.20minutos.es/horoscopo/'
-
-response = requests.get(url)
-response.raise_for_status()
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-table = soup.find("table", class_="data_wide_table new_bar_table")
-rows = table.find_all("tr")[1:]
-
-item_prices = []
-
-for row in rows:
-    columns = row.find_all("td")
-    item_name = columns[0].text.strip()
-    price_text = columns[1].text.strip().replace("USD", "").replace(",", "").strip()
+def get_soup(signo):
+     # URL base de página de horóscopos (signo específico)
+    url = f"https://www.20minutos.es/horoscopo/{signo}/"
     
-    try:
-        price = float(price_text)
-        item_prices.append((item_name, price))
-    except ValueError:
-        price = None 
+    # Realizar la solicitud HTTP
+    try:        
+        response = requests.get(url)
+        response.raise_for_status()  # Esto lanza una excepción si hay error
+    except requests.RequestException as e:
+        print(f"Error al obtener la página: {e}")
+        return None
+
+    return BeautifulSoup(response.text, "html.parser")
+
+def get_sign_description(soup: BeautifulSoup) -> str:
+    """Obtiene la descripción del signo"""
     
-    print(f"{item_name}: {price if price is not None else 'No disponible'}")
+    # Busca el div que contiene la descripción en el objeto soup
+    description = soup.find("div", {"class": "description"})
+    
+    # Convierte la descripción a texto
+    text = description.text.strip()
+    
+    return text
+
+def get_sign_prediction(soup: BeautifulSoup) -> str:
+    """Obtiene la predicción del signo"""
+
+    # Obtiene todos los div que contiene la clase "descripción" (hay dos en la página)
+    prediction_blocks = soup.find_all("div", {"class": "prediction"})
+    second_prediction_block = prediction_blocks[1]
+    specific_div = second_prediction_block.find("div", {"style": "padding-bottom: 1em;"})
+    prediction = specific_div.text.strip()
+    return prediction
+
+if __name__ == "__main__":
+    signo = "leo"
+    soup = get_soup(signo)
+    prediccion = get_sign_prediction(soup)
+    print(f"Predicción de: {signo}\n\n")
+    print(prediccion)
