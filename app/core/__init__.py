bd2ee3d1f0aba719ab4dc8b8c408316a4d03bdcc
anjivervas <anjivervas03@gmail.com>
2025-06-29 16:42:38 -0400

    Actualizada

---
+++ app/core/__init__.py
@@ -0,0 +1,2 @@
+from app.core.signos import Signos
+from app.core.anjiscrap import Horoscopo
--- app/core/anjiscrap.py
+++ app/core/anjiscrap.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import Opcional
+from app.core.signos import Signos
 
 class Horoscopo:
-    def __init__(self, signo: str):
+    def __init__(self, signo: Signos):
         self.signo = signo
         self.soup = self._get_soup()
         self.description = self._get_sign_description()
         self.prediction = self._get_sign_prediction()
 
-    def _get_soup(self):        
+    def _get_soup(self) -> Optional[BeautifulSoup]:         
         # Realizar la solicitud HTTP
         url = f"https://www.20minutos.es/horoscopo/{self.signo}/"
         try:        
@@ -20,27 +22,36 @@ class Horoscopo:
 
         return BeautifulSoup(response.text, "html.parser")
 
-    def _get_sign_description(self) -> str:
+    def _get_sign_description(self) -> Optional[str]:
         """Obtiene la descripción del signo"""
         
-        # Busca el div que contiene la descripción en el objeto soup
-        description = self.soup.find("div", {"class": "description"})
-        
-        # Convierte la descripción a texto
-        text = description.text.strip()
-        
-        return text
+         try:
+            # Busca el div que contiene la descripción en el objeto soup
+            description = self.soup.find("div", {"class": "description"})
+            # Convierte la descripción a texto
+            text = description.text.strip()
+            return text
+        except AttributeError:
+            print("Error al obtener la descripción del signo.")
+            return "Descripción no disponible."
 
-    def _get_sign_prediction(self) -> str:
+    def _get_sign_prediction(self) -> Optional[str]:
         """Obtiene la predicción del signo"""
 
-        # Obtiene todos los div que contiene la clase "descripción" (hay dos en la página)
-        prediction_blocks = self.soup.find_all("div", {"class": "prediction"})
-        second_prediction_block = prediction_blocks[1]
-        specific_div = second_prediction_block.find("div", {"style": "padding-bottom: 1em;"})
-        prediction = specific_div.text.strip()
-        return prediction
+       if not self.soup:
+            return "Predicción no disponible"
+
+        try:
+            # Obtiene todos los div que contiene la clase "descripción" (hay dos en la página)
+            prediction_blocks = self.soup.find_all("div", {"class": "prediction"})
+            second_prediction_block = prediction_blocks[1]
+            specific_div = second_prediction_block.find("div", {"style": "padding-bottom: 1em;"})
+            prediction = specific_div.text.strip()
+            return prediction
+        except (AttributeError, IndexError):
+            print("Error al obtener la predicción del signo.")
+            return None
 
 if __name__ == "__main__":
-    prediccion = Horoscopo("leo")
+    prediccion = Horoscopo(Signos.ARIES.signo())
     print(prediccion.prediction)
